ab9625d08c8ddbc0d85d5d22fbcf300e28b5a0a7
Dmitry <a89297175356@gmail.com>
2023-02-05 22:11:00 +0500

    5

--- src/JavaSwipper.java
+++ src/JavaSwipper.java
@@ -1,19 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
-
+import sweeeper.Box;
 public class JavaSwipper extends JFrame {
     private JPanel panel;
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
+
     public static void main(String[] args) {
     new JavaSwipper();
     }
     private JavaSwipper(){
+        setImages();
         initPanel();
         initFrame();
         }
 
     private void initPanel() {
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(500, 500));
+        panel = new JPanel(){
+            @Override                                       // AK
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Box box: Box.values())
+                g.drawImage((Image)box.image,
+                        box.ordinal()*IMAGE_SIZE, 0, this);
+
+            }
+        };
+
+        panel.setPreferredSize(new Dimension(COLS*IMAGE_SIZE, ROWS*IMAGE_SIZE));
         add (panel);
     }
     private void initFrame(){
@@ -24,7 +39,15 @@ public class JavaSwipper extends JFrame {
         setResizable(false);
         setVisible(true);
     }
-
+        private void setImages(){
+            for (Box box: Box.values())
+                box.image = getImage(box.name());
+        }
+        private Image getImage(String name){
+        String filename = "img/" + name.toLowerCase() + ".png";
+        ImageIcon icon = new ImageIcon (getClass().getResource(filename));
+        return icon.getImage();
+        }
 
 }
 
